7e2ad0335eb8ca1cb31e80dcaec08a26278dff7c
Muhammad Nabil Rafasya <mnabilrafasya03@gmail.com>
2025-08-26 21:04:51 +0700

    update env example

--- server/config/Database.js
+++ server/config/Database.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME,      // nama database, misal "bola"
-  process.env.DB_USER,      // user DB, misal "root"
-  process.env.DB_PASS,      // password DB (kosongkan jika tidak ada)
+  process.env.DB_NAME,      
+  process.env.DB_USER,      
+  process.env.DB_PASS,     
   {
     host: process.env.DB_HOST,  // misal "localhost"
     dialect: 'mysql',
